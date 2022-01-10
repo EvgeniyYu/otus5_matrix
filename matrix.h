bfd85f669c26e97033aea8c8beca4d0e669fcb64
Evgeniy <evgeniy.yurevich@gmail.com>
2022-01-10 21:27:47 +0300

    commit

--- matrix.h
+++ matrix.h
@@ -1,7 +1,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-#include <utility> 
+#include <utility>
 #include <cassert>
 #include <tuple>
 
@@ -13,7 +13,7 @@ struct Key
     Key(size_t _i, size_t _j): row(_i), col(_j) {}
     Key(const Key& k) {row = k.row; col = k.col;}
     bool operator== (const Key& k) const {return (k.row == row && k.col == col)? true: false;}
-    bool operator< (const Key& k) const 
+    bool operator< (const Key& k) const
     {
         if (row < k.row) return true;
         if (row > k.row) return false;
@@ -29,83 +29,82 @@ struct Element
     Element(const Key& _k, T _value): key(_k), value(_value) {}
 };
 
-template <class T, T default_value> 
+template <class T, T default_value>
 class MIterator;
 
-template <class T, T default_value> 
+template <class T, T default_value>
 class ProxyObject;
 
 
 template <class T, T default_value = -1>
 class Matrix
 {
- private:
+private:
     std::vector<Element<T>> v_values;
     friend class MIterator<T, default_value>;
-        
+
     class MRow
     {
     private:
-        Matrix<T, default_value>& m;        
+        Matrix<T, default_value>& m;
         size_t row;
     public:
-        MRow(Matrix<T, default_value>& _m, size_t _row): m(_m), row(_row) {} 
-        ProxyObject<T, default_value> operator[] (size_t col) 
+        MRow(Matrix<T, default_value>& _m, size_t _row): m(_m), row(_row) {}
+        ProxyObject<T, default_value> operator[] (size_t col)
         {
             return ProxyObject<T, default_value>(m, row, col);
         }
     };
-    
- public:
+
+public:
     Matrix()
     {
     }
-    
+
     MRow operator [](size_t row)
     {
         return MRow(*this, row);
     }
-    
+
     std::tuple<size_t, size_t, T> operator= (const Element<T>& e) const
     {
-        
         return std::make_tuple(e.key.row, e.key.col, e.value);
     }
-    
-    T get_value(size_t i, size_t j) 
+
+    T get_value(size_t i, size_t j)
     {
         const Key k(i, j);
         typename std::vector<Element<T>>::iterator it;
-        it = std::find_if(v_values.begin(), v_values.end(), [&k](const auto& e) {return (e.key == k)? true: false;});        
+        it = std::find_if(v_values.begin(), v_values.end(), [&k](const auto& e) {return (e.key == k)? true: false;});
         if (it != v_values.end())
             return it->value;
         else
             return default_value;
     }
-    
+
     void set_value(Element<T>& element)
     {
-    	typename std::vector<Element<T>>::iterator it;
+        typename std::vector<Element<T>>::iterator it;
         const Key k(element.key);
-        it = std::find_if(v_values.begin(), v_values.end(), [&k](const auto& e) {return (e.key == k)? true: false;});        
+        it = std::find_if(v_values.begin(), v_values.end(), [&k](const auto& e) {return (e.key == k)? true: false;});
         if (it != v_values.end())
         {
-        	if (element.value == default_value)	//free element in vector
-        		v_values.erase(it);
-        	else
-            	(*it).value = element.value;	//assign new value
+            if (element.value == default_value)	//free element in vector
+                v_values.erase(it);
+            else
+                (*it).value = element.value;	//assign new value
         }
         else
-        {    
-        	if (element.value != default_value)
-            	v_values.push_back(element);
+        {
+            if (element.value != default_value)
+                v_values.push_back(element);
         }
     }
 
-    
-    size_t size() const 
+
+    size_t size() const
     {
-    	return v_values.size();    
+        return v_values.size();
     }
 
     MIterator<T, default_value> begin()
@@ -113,13 +112,13 @@ class Matrix
         MIterator<T, default_value> it(*this);
         return it;
     }
-    
+
     MIterator<T, default_value> end()
     {
         MIterator<T, default_value> it(*this, v_values.size());
         return it;
     }
-        
+
     void print()
     {
         for (const auto& e: v_values)
@@ -134,22 +133,22 @@ template <class T, T default_value>
 class ProxyObject
 {
 private:
-	Matrix<T, default_value>& obj;
-	size_t row, col;
+    Matrix<T, default_value>& obj;
+    size_t row, col;
 public:
-	ProxyObject(Matrix<T, default_value>& _obj, size_t _row, size_t _col): obj(_obj), row(_row), col(_col) {}
-	
-	operator T ()
-	{
-		return obj.get_value(row, col);
-	}
-	
-	ProxyObject<T, default_value>& operator= (T val)
-	{
-		Element<T> e(Key(row, col), val);
-		obj.set_value(e);
-		return *this;
-	}
+    ProxyObject(Matrix<T, default_value>& _obj, size_t _row, size_t _col): obj(_obj), row(_row), col(_col) {}
+
+    operator T ()
+    {
+        return obj.get_value(row, col);
+    }
+
+    ProxyObject<T, default_value>& operator= (T val)
+    {
+        Element<T> e(Key(row, col), val);
+        obj.set_value(e);
+        return *this;
+    }
 };
 
 
@@ -162,30 +161,30 @@ private:
 public:
     MIterator(Matrix<T, default_value>& _m): m(_m), index(0) {}
     MIterator(Matrix<T, default_value>& _m, size_t _index): m(_m), index(_index) {}
-    
+
     void operator++ ()
     {
         index++;
     }
- 
+
     void operator-- ()
     {
         index--;
     }
- 
+
     bool operator== (const MIterator<T, default_value>& mIterator)
     {
         return index == mIterator.index;
     }
-    
+
     bool operator!= (const MIterator<T, default_value>& mIterator)
     {
         return index != mIterator.index;
     }
-     
+
     std::tuple<int, int, T> operator* ()
     {
         return std::make_tuple(m.v_values[index].key.row, m.v_values[index].key.col, m.v_values[index].value);
-    }    
+    }
 };
 
--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_MODULE test_matrix
 
-#include <boost/test/unit_test.hpp>	
+#include <boost/test/unit_test.hpp>
 #include <iostream>
 #include <sstream>
 #include "matrix.h"
@@ -9,16 +9,16 @@ BOOST_AUTO_TEST_SUITE(test_matrix)
 
 BOOST_AUTO_TEST_CASE(test_result)
 {
-    Matrix<int, -1> matrix;        
-    
+    Matrix<int, -1> matrix;
+
     auto a = matrix[0][0];
     BOOST_CHECK(a == -1);
-    BOOST_CHECK(matrix.size() == 0);    
-    
+    BOOST_CHECK(matrix.size() == 0);
+
     matrix[100][100] = 314;		//size = 1
     BOOST_CHECK(matrix[100][100] == 314);
     BOOST_CHECK(matrix.size() == 1);
-    
+
     matrix[10][10] = 300;	//size = 2
     matrix[50][50] = 350;	//size = 3
     BOOST_CHECK(matrix[10][10] == 300);
@@ -29,7 +29,7 @@ BOOST_AUTO_TEST_CASE(test_result)
     BOOST_CHECK(c == 300);
     BOOST_CHECK(matrix.size() == 3);
     BOOST_CHECK(matrix[11][10] == -1);
-    
+
     matrix[50][50] = 400;	//size = 3
     BOOST_CHECK(matrix[50][50] == 400);
     matrix[50][50] = -1;
@@ -38,20 +38,16 @@ BOOST_AUTO_TEST_CASE(test_result)
     std::stringstream out;
     for (const auto& c: matrix)
     {
-        int x;
-        int y;
-        int v;
+        int x, y, v;
         std::tie(x, y, v) = c;
-        
         out << x << y << v;
     }
-	BOOST_CHECK(out.str() == "1001003141010300");	
-	
+    BOOST_CHECK(out.str() == "1001003141010300");
 }
 
 BOOST_AUTO_TEST_CASE(test_canonical_form)
 {
-    Matrix<int, 0> matrix;        
+    Matrix<int, 0> matrix;
     ((matrix[100][100] = 314) = 0) = 217;
     BOOST_CHECK(matrix[100][100] == 217);
 }
